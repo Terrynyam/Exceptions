69cd9d9c7070fc3316d7e5c46c0cea0d77c4e412
terrancen <terrancen@afrosoft.co.zw>
2024-06-07 14:26:32 +0200

    Initial Commit

---
+++ src/main/java/com/tenic/exceptions/ExceptionApplication.java
@@ -0,0 +1,16 @@
+//package com.tenic.exceptions;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @author Terrance Nyamfukudza
+// * 7/6/2024
+// */
+//@SpringBootApplication
+//public class ExceptionApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(ExceptionApplication.class, args);
+//    }
+//}
